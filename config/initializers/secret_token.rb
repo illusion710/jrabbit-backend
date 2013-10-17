757750e5d0d1c8655064dedb45b5bf8f2e7b6933
Hyoungook Bae <illusion710@gmail.com>
2013-10-17 15:40:02 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JrabbitBackend::Application.config.secret_key_base = 'ee9036590e465fc7becae9ae7d639ca411fae71a095f7f229c2718f826f9685406857c0ebbc27aba745b46d07cd18d944403b2c0396914e7448c765b47fdefda'
